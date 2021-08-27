40bf5e8fcaf9dcc7f9a782bc861e93a8ade47bb5
teenastewart <teenastewart@gmail.com>
2021-08-26 19:46:52 -0500

    updating python file

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
-import pandas as pd
+import numpy as np
+from config import password
+from config import username
+import psycopg2
+# import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify, render_template
+
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName("Causes").getOrCreate()
